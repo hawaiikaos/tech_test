dc0bc23d56f2510d452b8f59823d52e6b21f3c17
Katharine Osborne <hawaiikaos@gmail.com>
2024-07-16 22:50:50 +0100

    Make board clickable

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -9,11 +9,19 @@ export const Main = () => {
     [undefined, undefined, undefined]
   ])
 
+  const takeTurn = (row:number, col:number) => {
+    console.log('in takeTurn');
+
+    // only mark square if it's unoccupied
+    // AND there isn't a winner
+  }
+
   return <div className='flex flex-col mt-10 items-center gap-10'>
     <div className='font-bold text-2xl'>Tic Tac Toe</div>
     <div className='flex flex-col gap-1'>
-      {board.map(row => <div className='flex gap-1'>
-        {row.map(column => <div className='border-2 border-gray-900 w-10 h-10 cursor-pointer items-center justify-center text-2xl font-bold flex'>
+
+      {board.map((row, rowindex) => <div className='flex gap-1'>
+        {row.map((column, columnindex) => <div onClick={()=>takeTurn(rowindex, columnindex)} key={rowindex+columnindex} className='border-2 border-gray-900 w-10 h-10 cursor-pointer items-center justify-center text-2xl font-bold flex'>
           {column}
         </div>)}
       </div>)}
